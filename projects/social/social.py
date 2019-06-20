7a37a25a82658029f129a6986cf8953895ffe650
Erin Koen <erin.koen@gmail.com>
2019-06-20 12:52:16 -0400

    refactored the add friendship part to better reflect the average number of friends

--- projects/social/social.py
+++ projects/social/social.py
@@ -66,27 +66,39 @@ class SocialGraph:
             self.addUser(User(user))
 
         # Create friendships
-        # create a randomized array of potential friends, and slice off a random number between 0 and 2*num inclusive of those friends from the front. Loop through that second array (here is the scaling issue) and call the add friendships
-        for userId in self.users.keys():  # O(n)
-            #eliminate friends with yourself
-            newUsers.remove(userId)
-            #randomize the rest of the array
-            random_friends = FYS(newUsers)  # O(n)
 
-            random_number = random.randint(0, 2*avgFriendships)
-            # in the case userId has no friends
-            if random_number == 0:
-                pass 
-            # loop through the randomized array the random number of times. Evenly distributed psuedorandom numbers between 0 and n should average out to n/2
-            else:
-                for i in range(0, random_number):  # O(n)
-                    if random_friends[i] in self.friendships[userId] or user in self.friendships[random_friends[i]]:
-                        pass
-                    else:
-                        self.friendships[userId].add(random_friends[i])
-                        self.friendships[random_friends[i]].add(userId)
-                    # print('test', self.friendships[userId])
-            newUsers.append(userId)
+        # Create an array of all possible friendships. Shuffle the array. Add friendships in an amount = num users * avg friendships // 2
+        possible_friendships = []
+        for userID in self.users:
+            for friendID in range(userID + 1, self.lastID+1):
+                possible_friendships.append((userID, friendID))
+        FYS(possible_friendships)
+        friendships_to_create = possible_friendships[:numUsers*avgFriendships //2]
+
+        for pair in friendships_to_create:
+            self.addFriendship(pair[0],pair[1])
+
+        # create a randomized array of potential friends, and slice off a random number between 0 and 2*num inclusive of those friends from the front. Loop through that second array (here is the scaling issue) and call the add friendships
+        # for userId in self.users.keys():  # O(n)
+        #     #eliminate friends with yourself
+        #     newUsers.remove(userId)
+        #     #randomize the rest of the array
+        #     random_friends = FYS(newUsers)  # O(n)
+
+        #     random_number = random.randint(0, 2*avgFriendships)
+        #     # in the case userId has no friends
+        #     if random_number == 0:
+        #         pass 
+        #     # loop through the randomized array the random number of times. Evenly distributed psuedorandom numbers between 0 and n should average out to n/2
+        #     else:
+        #         for i in range(0, random_number):  # O(n)
+        #             if random_friends[i] in self.friendships[userId] or user in self.friendships[random_friends[i]]:
+        #                 pass
+        #             else:
+        #                 self.friendships[userId].add(random_friends[i])
+        #                 self.friendships[random_friends[i]].add(userId)
+        #             # print('test', self.friendships[userId])
+        #     newUsers.append(userId)
 
     def getAllSocialPaths(self, userID):
         """
@@ -105,12 +117,13 @@ class SocialGraph:
         while q.empty() is False:
             path = q.get()
             v = path[-1]
-            for friend in self.friendships[v]:
-                if friend not in visited:
-                    visited[friend] = path
-                    path_copy = list(path)
-                    path_copy.append(friend)
-                    q.put(path_copy)
+            if v not in visited:
+                visited[v]=path
+                for friend in self.friendships[v]:
+                    if friend not in visited:
+                        path_copy = list(path)
+                        path_copy.append(friend)
+                        q.put(path_copy)
            
 
         return visited
@@ -118,7 +131,7 @@ class SocialGraph:
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populateGraph(20, 5)
-    print(sg.friendships)
+    sg.populateGraph(100, 5)
+    # print(sg.friendships)
     connections = sg.getAllSocialPaths(1)
     print(connections)
